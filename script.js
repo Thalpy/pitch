425c3f44f0cc6189d03c458f5918ce6cd554e216
Thalpy <33956696+Thalpy@users.noreply.github.com>
2023-08-25 03:01:33 +0100

    How did I make this mistake

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function getCookie(name, defaultval)
   var ca = document.cookie.split(';');
   var nameEQ = name + "=";
   for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];const signalExists = maybeAmplitude > 0.5;
+    var c = ca[i];
     while (c.charAt(0) == ' ') c = c.substring(1, c.length); //delete spaces
     if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
   }
